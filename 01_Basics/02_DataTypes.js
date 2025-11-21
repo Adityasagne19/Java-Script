eb643ed2d9b7cb0ab76a589494b22e8b526e6908
Aditya sagne <165883282+Adityasagne19@users.noreply.github.com>
2025-11-21 16:56:41 +0000

    DataTypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,27 @@
+"Use Strict";// Treat all js code as newer version 
+
+//alert(3+3) We are using nodejs, not a browser 
+
+console.log(3
+    +
+    3)// Code Readability Should be high not like this 
+
+console.log("Aditya")
+
+//Data Types 
+let name = "Aditya"       //String
+let age=21               //Int
+let isLoggedIn = false  //Boolean
+let state;             //Undefined
+
+//number = 2 to power53
+//bigInt = very large number only 
+//String = ""
+//Boolean = True/False
+//null = Standalone value 
+//undefined = 
+console.log(typeof undefined);  // Output = Undefined
+//Symbol = Unique
+
+//Object 
+console.log(typeof null); // Output = Object 
