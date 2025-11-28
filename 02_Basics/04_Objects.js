c8b078ed0dfc0ca4313a5924e0690f4d5f9e1f38
Aditya sagne <165883282+Adityasagne19@users.noreply.github.com>
2025-11-28 12:24:27 +0000

    More Addition to Objects

--- 02_Basics/04_Objects.js
+++ 02_Basics/04_Objects.js
@@ -17,13 +17,73 @@ fullname: {
     }
 }
 
-console.log(regularUser.fullname.userfullname.firstname);
+//console.log(regularUser.fullname.userfullname.firstname);
 
 
 const obj1 = {1:"a",2:"b"}
 const obj2 = {2:"a",3:"b"}
 const obj3 = {4:"a",5:"b"}
 
-const obj4 = Object.assign({},obj1,obj2,obj3)   //Copy the values of all of the enumerable own properties from one or more source objects to a target object. Returns the target object.
-console.log(obj4);
+// const obj4 = Object.assign({},obj1,obj2,obj3)   //Copy the values of all of the enumerable own properties from one or more source objects to a target object. Returns the target object.
+// console.log(obj4);
+
+const obj4 = {...obj1,...obj2,...obj3}             //Using the Spread Operator (...)
+// console.log(obj4);
+
+const users = [                                    //Arrays of Objects 
+    {
+        id: "123",
+        email: "users@gmail.com"
+    },
+    {
+        id: "123",
+        email: "users@gmail.com"
+    },
+    {
+        id: "123",
+        email: "users@gmail.com"
+    }
+]
+users[1].email;                                    //Accessing  a Object in a Array.
+
+//Using Some Object Methods 
+
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));              //Gives the keys in form of an Array
+// console.log(Object.values(tinderUser));            //Gives the values in form of an Array
+// console.log((Object.entries(tinderUser)));          //Gives both     in form of an Array
+
+
+const  Course = {
+    CourseName : "JS In Hindi",
+    Price : "Free",
+    CourseInstructor : "Aditya"
+}
+
+Course.CourseInstructor                                 //This is a long way of accessing CourseInstructor.
+
+//Another Clean AND Short way 
+
+const {CourseInstructor : Instructor} = Course;
+
+//console.log(CourseInstructor);                          //Now only Need to Log it every time we need to  use it.
+//console.log(Instructor);                                //We can also make a shorter name for it.
+
+
+//JSON API
+{
+    "name" = "Aditya",
+    "courseName" = "JS",
+    "price" = "Free"
+}
+
+
+//You can Also Get APIs in for of Array.
+//Like this.
+[                                                   //To Formate this type of data there are whereas formatters 
+    {},
+    {},
+    {}
+
+]
 
