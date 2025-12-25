3145ec193bb5bba699085934c09826ac8b258e8f
Aditya sagne <165883282+Adityasagne19@users.noreply.github.com>
2025-12-25 06:21:11 +0000

    ForEach

--- 05_Iterations/Five.js
+++ 05_Iterations/Five.js
@@ -1,6 +1,57 @@
 const coding = ["js", "py", "java", "c++", "rb"];
 
 coding.forEach( function (val) {
-    console.log(val);
+   // console.log(val);
 })
 
+// Another way to write the same code using arrow functions
+
+coding.forEach( (item) => {
+    // console.log(item);
+})
+
+// Even more concise version
+
+function printMe(item) {
+   // console.log(item);
+}
+
+coding.forEach(printMe);
+
+// Full version with all parameters
+
+coding.forEach((item , index , arr) => {
+   // console.log(item, index, arr);
+});
+
+// Example with objects in an array 
+
+const myCoding = [
+    {
+         languageName: "JavaScript",
+            fileExtension: "js"
+    },
+    {
+         languageName: "Python",
+            fileExtension: "py"
+    },
+    {
+         languageName: "Java",
+            fileExtension: "java"
+    },
+    {
+         languageName: "C++",
+            fileExtension: "cpp"
+    },
+    {
+         languageName: "Ruby",
+            fileExtension: "rb"
+    }
+]
+
+// Accessing objects in the array using forEach
+
+myCoding.forEach( (item) => {
+    console.log(item.fileExtension);
+
+})
